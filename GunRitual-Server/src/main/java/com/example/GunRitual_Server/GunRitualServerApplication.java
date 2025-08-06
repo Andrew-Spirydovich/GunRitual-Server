c27d974948df764b0b8c4bd29c94231b60803813
Andrew-Krit <salandrey833@gmail.com>
2025-08-07 01:12:12 +0300

    Initial commit

---
+++ GunRitual-Server/src/main/java/com/example/GunRitual_Server/GunRitualServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.GunRitual_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunRitualServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunRitualServerApplication.class, args);
+	}
+
+}
